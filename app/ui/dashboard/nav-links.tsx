7f61c216d8d0db81cc92624a6c58aaa2a7b810ba
TanujRathore <mr733185@gmail.com>
2025-12-28 10:11:22 +0530

    modified nav link

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -16,8 +16,8 @@ const links = [
     name: 'Invoices',
     href: '/dashboard/invoices',
     icon: DocumentDuplicateIcon,
-  },
-  { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
+  }
+  //{ name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
 ];
 
 export default function NavLinks() {
